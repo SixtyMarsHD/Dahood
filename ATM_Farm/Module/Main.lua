87c800dd8602949316db629536287f265924d860
SixtyMarsHD <minecraftcpvp1@gmail.com>
2025-11-22 08:22:10 +0000

    update

--- ATM_Farm/Module/Main.lua
+++
@@ -1,4 +0,0 @@
-local ATMScript = loadstring(game:HttpGet("https://github.com/SixtyMarsHD/Dahood/blob/main/ATM_Farm/Module/ATMScript.lua"))()
-local AdminScript = loadstring(game:HttpGet("https://github.com/SixtyMarsHD/Dahood/blob/main/ATM_Farm/Module/AdminCMD.lua"))()
-
-
